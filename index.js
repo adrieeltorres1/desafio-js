2845cdc507b804ae578f45b79ce3c1d3958025cc
adrieeltorres1 <adrieltorreslds@gmail.com>
2024-09-30 23:22:06 -0300

    fix: Problema: Verificar se um número é par ou ímpar

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 
-// Condicional para velocida//
+// Condicional para velocidade//
 
-// let velocidade = 100;
+// let velocidade = prompt('Qual a velocidade?');
 
 // if (velocidade >=50) {
 //     console.log('Multado!')
@@ -13,30 +13,38 @@
 
 //Escolher lado de um triângulo//
 
-let LadoUm = prompt('Insira o primeiro valor do triângulo');
+// let LadoUm = prompt('Insira o primeiro valor do triângulo');
 
-let LadoDois =  prompt('Insira o segundo lado do triângulo');
+// let LadoDois =  prompt('Insira o segundo lado do triângulo');
 
-let LadoTres = prompt('Insira o terceiro lado do triângulo');
+// let LadoTres = prompt('Insira o terceiro lado do triângulo');
 
-if (LadoUm === LadoDois && LadoDois === LadoTres) {
-    document.write('Este triângulo é um Equilátero');
-} 
-else if (LadoUm === LadoDois ||  LadoUm === LadoTres || LadoDois ===LadoTres ){
-    document.write('Este Triangulo é um Isósceles');
-} else {
-    document.write('Este triângulo é um Escaleno');
-}
+// if (LadoUm === LadoDois && LadoDois === LadoTres) {
+//     document.write('Este triângulo é um Equilátero');
+// } 
+// else if (LadoUm === LadoDois ||  LadoUm === LadoTres || LadoDois ===LadoTres ){
+//     document.write('Este Triangulo é um Isósceles');
+// } else {
+//     document.write('Este triângulo é um Escaleno');
+// }
 
 
 //Problema: Verifique se um número é positivo, negativo ou zero.//
 
-let numero = parseFloat(prompt('Digite um número'));
+// let numero = parseFloat(prompt('Digite um número'));
 
-if (numero > 0) {
-    document.write('Este número é positivo.');
-}else if (numero < 0) {
-    document.write('O número é negativo')
+// if (numero > 0) {
+//     document.write('Este número é positivo.');
+// }else if (numero < 0) {
+//     document.write('O número é negativo')
+// }
+
+//Problema: Verificar se um número é par ou ímpar.//
+
+let numeros = prompt('Digite um número inteiro')
+
+if (numeros % 2 === 0)  {
+    document.write('Este número é par')
+}else {
+    document.write('Este número é ímpar')
 }
-
-
