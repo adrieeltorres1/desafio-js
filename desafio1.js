050ab7c53ba739a8b10499e440f977bebe3822ab
adrieeltorres1 <adrieltorreslds@gmail.com>
2024-10-02 12:02:03 -0300

    Escolher lado de um triângulo

--- desafio1.js
+++ desafio1.js
@@ -2,11 +2,11 @@
 
 //Escolher lado de um triângulo//
 
-let LadoUm = prompt('Insira o primeiro valor do triângulo');
+let LadoUm = prompt('Insira o primeiro valor');
 
-let LadoDois =  prompt('Insira o segundo lado do triângulo');
+let LadoDois =  prompt('Insira o segundo valor');
 
-let LadoTres = prompt('Insira o terceiro lado do triângulo');
+let LadoTres = prompt('Insira o terceiro valor');
 
 if (LadoUm === LadoDois && LadoDois === LadoTres) {
     document.write('Este triângulo é um Equilátero');
